0dbc4dd7366d400bb055fe15aa995041e8016cb2
Sahil Rai <sahilatwork21@gmail.com>
2025-09-26 00:22:42 +0530

    Factorial trailing zeroes
    Given an integer n, return the number of trailing zeroes in n!.

---
+++ leetcode172.cpp
@@ -0,0 +1,23 @@
+/*
+class Solution {
+
+private:
+    int fact(int n){
+        if(n == 0 || n == 1) return 1;   //base case
+        return n * fact(n - 1);          //recursive call
+    }  
+
+public:
+    int trailingZeroes(int n) {
+
+        int factorial = fact(n);       //compute factorial
+
+        int count = 0;
+        while(factorial % 10 == 0) {      //count trailing zeroes
+            count++;
+            factorial = factorial / 10;
+            }
+        return count;
+    }
+};
+*/
