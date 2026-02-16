2e18c7f18e05b46965ab50d6dcb4efcf609f3e37
Sahil Rai <sahilatwork21@gmail.com>
2026-02-16 21:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (42.66%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,40 +1,61 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//APPROACH 2 : TC - O(N)
 class Solution {
-private:
-    int height(TreeNode* root){
-        //base case
-        if(root == NULL){
-            return 0;
-        }
-
-        int left = height(root->left);
-        int right = height(root->right);
-
-        return max(left, right) + 1;
-    }   
-
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
+    pair<int, int> diameterFast(TreeNode* root){        
+        //pair first part represents diameter & second represents height
         //base case
         if(root == NULL){
-            return 0;
+            pair<int, int> p = make_pair(0,0);
+            return p;
         }
-        
-        int op1 = diameterOfBinaryTree(root->left);  //diameter is in left subtree
-        int op2 = diameterOfBinaryTree(root->right);  //diameter is in right subtree
-        int op3 = height(root->left) + height(root->right);
+        pair<int, int> left = diameterFast(root->left);
+        pair<int, int> right = diameterFast(root->right);
 
-        int ans = max(op1, max(op2, op3));
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second;
+
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second, right.second) + 1;
+
         return ans;
     }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameterFast(root).first;    //returns first part of ans which represents diameter 
+    }
 };
+
+
+
+
+
+//APPROACH-1 : TC - O(N*N) : 
+//HEIGHT IS CALLED INSIDE DIAMETER & IN WC BOTH FUNCTIONS ITERATES THE WHOLE TREE WHICH HAS N NODES
+// class Solution {
+// private:
+//     int height(TreeNode* root){
+//         //base case
+//         if(root == NULL){
+//             return 0;
+//         }
+//         int left = height(root->left);
+//         int right = height(root->right);
+
+//         return max(left, right) + 1;
+//     }   
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         //base case
+//         if(root == NULL){
+//             return 0;
+//         }
+//         int op1 = diameterOfBinaryTree(root->left);  //diameter is in left subtree
+//         int op2 = diameterOfBinaryTree(root->right);  //diameter is in right subtree
+//         int op3 = height(root->left) + height(root->right);
+
+//         int ans = max(op1, max(op2, op3));
+//         return ans;
+//     }
+// };
