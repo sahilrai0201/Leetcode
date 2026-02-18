010442cc567a39c2f562b5e82ec83e4f2768dac3
Sahil Rai <sahilatwork21@gmail.com>
2026-02-18 19:22:50 +0530

    Time: 34 ms (18.39%) | Memory: 34.6 MB (20.02%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,54 @@
+//APPROACH-1 : 
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> count;
+        int missing, duplicate;
+
+        //count freq of each number
+        for(int num : nums){
+            count[num]++;
+        }
+
+        //find duplicate and missing numbers
+        for(int i=1; i<=n; i++){
+            if(count[i] == 2){
+                duplicate = i;
+            }
+            else if(count[i] == 0){
+                missing = i;
+            }
+        }
+        return {duplicate, missing};
+    }
+};
+
+
+
+//APPROACH-2 : 
+// class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         int dup = -1;
+//         int missing = -1;
+
+//         for(int i=0; i<n; i++){
+//             if(nums[abs(nums[i]) - 1] < 0){
+//                 dup = abs(nums[i]);
+//             } else {
+//                 nums[abs(nums[i]) - 1] = (-1) * nums[abs(nums[i]) - 1];
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > 0){
+//                 missing = (i+1);
+//                 break;
+//             }
+//         }
+//         return {dup, missing};
+//     }
+// };
