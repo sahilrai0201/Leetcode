32894eb45e722f0cf99c432fd60dc81222b4bc35
Sahil Rai <sahilatwork21@gmail.com>
2026-04-03 00:24:25 +0530

    Time: 24 ms (5.44%) | Memory: 24.4 MB (7.71%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
+        
+        // Map to store frequency of each number
+        map<int, int> mp;
 
-        //XOR all elements in the array!
+        // Traverse the array and count occurrences of each element
         for(int x : nums){
-            ans = ans ^ x;
+            mp[x]++;   // increment count of element x
         }
-        return ans;
+
+        // Iterate through the map to find the element with frequency 1
+        for(auto i : mp){
+            // i.first  -> element
+            // i.second -> frequency of that element
+            if(i.second == 1){
+                return i.first;   // return the element that appears only once
+            }
+        }
+
+        // In case no unique element is found (though problem guarantees one)
+        return -1; 
     }
-}; 
+};
