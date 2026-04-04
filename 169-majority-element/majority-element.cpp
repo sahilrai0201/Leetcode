d2a1f18c1752c7f0660401460b9baf2b41ee1f6c
Sahil Rai <sahilatwork21@gmail.com>
2026-04-04 22:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (27.77%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,42 @@
+//APPROACH 2 : 
+//TC = O(n log n) and SC = O(n) : 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        int candidate = nums[0], vote = 1;
-        for(int i=1; i<nums.size(); i++){
-            if(vote == 0) {
-                candidate = nums[i];
-                vote = 1;
-            }
-            else if(nums[i] == candidate) {
-                vote++;
-            }
-            else{
-                vote--;
+        int n = nums.size();
+        map<int, int> mp;
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        for(auto j : mp){
+            if(j.second > n/2){
+                return j.first;
             }
         }
-        return candidate;
+        return -1;    //just in-case
     }
 };
+
+
+//APPROACH 1: 
+// TC = O(n) and SC = O(1) :
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+
+//         int candidate = nums[0], vote = 1;
+//         for(int i=1; i<nums.size(); i++){
+//             if(vote == 0) {
+//                 candidate = nums[i];
+//                 vote = 1;
+//             }
+//             else if(nums[i] == candidate) {
+//                 vote++;
+//             }
+//             else{
+//                 vote--;
+//             }
+//         }
+//         return candidate;
+//     }
+// };
