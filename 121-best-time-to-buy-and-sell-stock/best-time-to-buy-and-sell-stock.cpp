b4199f42af0ada72f2f386144790cd1f4b61d8e7
Sahil Rai <sahilatwork21@gmail.com>
2026-04-07 00:55:01 +0530

    Time: 3 ms (27.92%) | Memory: 97.2 MB (86.58%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,7 @@
+//OPTIMISED APPROACH : TC = O(N) & SC = O(1)---------------->
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-
         int min_price = INT_MAX;
         int profit = 0;
 
@@ -12,3 +12,23 @@ public:
         return profit;
     }
 };
+
+
+
+//BRUTE FORCE : TC = O(N*N) & SC = O(1) -------------->
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         int currProfit = 0;
+//         int maxProfit = 0;
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 currProfit = prices[j] - prices[i];
+//                 maxProfit = max(maxProfit, currProfit);
+//             }
+//         }
+//         return maxProfit;
+//     }
+// };
