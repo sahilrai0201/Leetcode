73b6462f94146553ffe0976d23ccb2ddd347101f
Sahil Rai <sahilatwork21@gmail.com>
2026-04-09 20:43:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.50%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,16 @@
+//TC = O(N) & SC = O(1)----------->
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int ans = 0;
         int n = nums.size();
-
+        
         //XOR all elements from 0 to n
         for(int i=0; i<=n; i++){
             ans = ans ^ i;
         }
 
-        //XOR all elements in the array!
+        //XOR all elements of array
         for(int x : nums){
             ans = ans ^ x;
         }
