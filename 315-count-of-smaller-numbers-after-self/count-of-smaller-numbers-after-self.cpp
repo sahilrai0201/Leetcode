12c0ed2977a2dc914988b65f4116671dc90c4aeb
Sahil Rai <sahilatwork21@gmail.com>
2026-04-28 12:21:54 +0530

    Time: 811 ms (42.33%) | Memory: 425.5 MB (34.52%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,60 +1,87 @@
+//TC = O(NlogN) & SC = O(N)------------------------------------------------------->
 class Solution {
 public:
-    vector<int> ans;
-    vector<int> index;
+    vector<int> ans;     // final answer
+    vector<int> index;   // stores original positions
 
-    void mergeSort(vector<int>& nums, int left, int right){
-        if(left >= right) return;
+    // Merge two sorted parts
+    void merge(vector<int>& nums, int left, int mid, int right) {
 
-        int mid = (left + right)/2;
+        vector<int> temp;      // temporary array for sorted indices
 
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid+1, right);
+        int i = left;          // start of left half
+        int j = mid + 1;      // start of right half
 
-        merge(nums, left, mid, right);
-    }
+        int rightCount = 0;   // how many smaller right elements passed
 
-    void merge(vector<int>& nums, int left, int mid, int right){
-        vector<int> temp;
-        int i = left, j = mid + 1;
-        int rightCount = 0;
+        // compare both halves
+        while (i <= mid && j <= right) {
 
-        while(i <= mid && j <= right){
-            if(nums[index[j]] < nums[index[i]]) {
-                temp.push_back(index[j]);
-                rightCount++;
+            // if right value is smaller
+            if (nums[index[j]] < nums[index[i]]) {
+
+                temp.push_back(index[j]); // put right index
+                rightCount++;             // one smaller found
                 j++;
+
             } else {
+
+                // add count to left element
                 ans[index[i]] += rightCount;
-                temp.push_back(index[i]);
+
+                temp.push_back(index[i]); // put left index
                 i++;
             }
         }
 
-        while(i <= mid) {
+        // remaining left elements
+        while (i <= mid) {
+
             ans[index[i]] += rightCount;
             temp.push_back(index[i]);
             i++;
         }
 
-        while(j <= right) {
+        // remaining right elements
+        while (j <= right) {
+
             temp.push_back(index[j]);
             j++;
         }
 
-        for(int k = left; k <= right; k++) {
+        // copy back into index array
+        for (int k = left; k <= right; k++) {
             index[k] = temp[k - left];
         }
     }
 
+    // Normal merge sort
+    void mergeSort(vector<int>& nums, int left, int right) {
+
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+
+        mergeSort(nums, left, mid);       // left half
+        mergeSort(nums, mid + 1, right);  // right half
+
+        merge(nums, left, mid, right);    // merge both
+    }
+
     vector<int> countSmaller(vector<int>& nums) {
+
         int n = nums.size();
-        ans.assign(n, 0);
+
+        ans.assign(n, 0);   // all answers 0
         index.resize(n);
 
-        for(int i=0; i<n; i++) index[i] = i;
+        // store original positions
+        for (int i = 0; i < n; i++) {
+            index[i] = i;
+        }
 
-        mergeSort(nums, 0, n-1);
+        mergeSort(nums, 0, n - 1);
+
         return ans;
     }
 };
