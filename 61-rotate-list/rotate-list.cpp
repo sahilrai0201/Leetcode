96aeb02bff1c7ee45368d74485d007c0ec889528
Sahil Rai <sahilatwork21@gmail.com>
2026-05-05 12:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (31.20%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,46 +1,89 @@
-//BETTER SOLUTION ----------------------------------------------------->
-//TC = O(K*N) & SC = O(1) -------------------------------------------------->
+//APPROACH 3 : OPTIMISED SOLUTION ----------------------------------------------->
+//TC = O(N) & SC = O(1) --------------------------------------------------------->
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head || !head->next || k == 0) return head;
 
-        //Step 1 : Find length
-        int n = 0;
+        //Step 1 : Find length and last node
         ListNode* temp = head;
-        while(temp != NULL){
-            n++;
+        int n = 1;
+
+        while(temp->next != NULL){
             temp = temp->next;
+            n++;
         }
 
         //Step 2 : Reduce k
         k = k % n;
-        if(k == 0) return head;
+        if(k == 0) return head;   //no rotation needed
 
-        //Step 3 : Same brute force logic
-        for(int i=0; i<k; i++){
-            ListNode* temp = head;
-            ListNode* prev = NULL;
+        //Step 3 : Make Circular
+        temp->next = head;
 
-            while(temp->next != NULL){
-                prev = temp;
-                temp = temp->next;
-            }
+        //Step 4 : Find new tail (n-k steps)
+        int steps = n-k;
+        ListNode* newTail = head;
 
-            prev->next = NULL;
-            temp->next = head;
-            head = temp;
+        for(int i=1; i<steps; i++){
+            newTail = newTail->next;
         }
 
-        return head;
+        //Step 5 : Break the circle
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
     }
 };
 
 
 
 
-//BRUTE FORCE ----------------------------------------------------->
-//TC = O(K*N) & SC = O(1) -------------------------------------------------->
+//APPROACH 2 : BETTER SOLUTION ----------------------------------------------------->
+//TC = O(N*N) & SC = O(1) ---------------------------------------------------------->
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         if(!head || !head->next || k == 0) return head;
+
+//         //Step 1 : Find length
+//         int n = 0;
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             n++;
+//             temp = temp->next;
+//         }
+
+//         //Step 2 : Reduce k
+//         k = k % n;
+//         if(k == 0) return head;
+
+//         //Step 3 : Same brute force logic
+//         for(int i=0; i<k; i++){
+//             ListNode* temp = head;
+//             ListNode* prev = NULL;
+
+//             while(temp->next != NULL){
+//                 prev = temp;
+//                 temp = temp->next;
+//             }
+
+//             prev->next = NULL;
+//             temp->next = head;
+//             head = temp;
+//         }
+
+//         return head;
+//     }
+// };
+
+
+
+
+
+//APPROACH 1 : BRUTE FORCE ----------------------------------------------------->
+//TC = O(K*N) & SC = O(1) ------------------------------------------------------>
 // class Solution {
 // public:
 //     ListNode* rotateRight(ListNode* head, int k) {
