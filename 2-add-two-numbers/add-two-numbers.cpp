73f48ca6f4d0fdb0aab0e6392c002051acdc80c9
Sahil Rai <sahilatwork21@gmail.com>
2026-07-08 21:50:26 +0530

    Time: 1 ms (45.37%) | Memory: 77.2 MB (12.65%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 private:
     void insertAtTail(ListNode* &head, ListNode* &tail, int val){
@@ -24,12 +14,13 @@ private:
         }
     }
 
-    ListNode* add(ListNode* l1, ListNode* l2){
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         ListNode* ansHead = NULL;
         ListNode* ansTail = NULL;
 
-        while(l1!=NULL || l2!=NULL || carry!=0){
+        while(l1 != NULL || l2 != NULL || carry != 0){
             int val1 = 0;
             if(l1 != NULL) val1 = l1->val;
 
@@ -47,11 +38,5 @@ private:
             if(l2 != NULL) l2 = l2->next;
         }
         return ansHead;
-    }   
-
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = add(l1, l2);
-        return ans;
     }
 };
