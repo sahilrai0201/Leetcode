2790f7366a32c8943f8897f5cf192278ed391739
Sahil Rai <sahilatwork21@gmail.com>
2026-07-23 21:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (63.04%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,53 +1,68 @@
-//APPROACH 2 : TC = O(N*M) & SC = O(N)--------------------------------------->
+//APPROACH 1 : TC = O(N+M) & SC = O(N+M)--------------------------------------->
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
 
-        for(int i=0; i<nums1.size(); i++){
-            int j;
-            for(j=0; j<nums2.size(); j++){
-                if(nums2[j] == nums1[i]){
-                    break;
-                }
-            }    
-            int nextGreater = -1;
-            for(int k=j+1; k<nums2.size(); k++){
-                if(nums2[k] > nums1[i]){
-                    nextGreater = nums2[k];
-                    break;
-                }
+        stack<int> st;
+        unordered_map<int, int> mp;   // Stores: element -> next greater element
+
+        // Traverse from right to left
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            // Remove all smaller or equal elements
+            // They can never be the next greater element
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
-            result.push_back(nextGreater);
+
+            // If stack is empty, no greater element exists
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            }
+            // Otherwise, top is the next greater element
+            else {
+                mp[nums2[i]] = st.top();
+            }
+
+            // Push current element for future comparisons
+            st.push(nums2[i]);
         }
-        return result;
+
+        vector<int> ans;
+
+        // Fetch answers for nums1 from the map
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
     }
 };
 
 
-//APPROACH 1 : TC = O(N+M) & SC = O(N+M)--------------------------------------->
+
+//APPROACH 2 : TC = O(N*M) & SC = O(N)--------------------------------------->
 // class Solution {
 // public:
 //     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//         stack<int> st;
-//         unordered_map<int,int> mp;
+//         vector<int> result;
 
-//         for(int i=nums2.size()-1; i>=0; i--){
-//             while(!st.empty() && st.top() <= nums2[i]){
-//                 st.pop();
+//         for(int i=0; i<nums1.size(); i++){
+//             int j;
+//             for(j=0; j<nums2.size(); j++){
+//                 if(nums2[j] == nums1[i]){
+//                     break;
+//                 }
+//             }    
+//             int nextGreater = -1;
+//             for(int k=j+1; k<nums2.size(); k++){
+//                 if(nums2[k] > nums1[i]){
+//                     nextGreater = nums2[k];
+//                     break;
+//                 }
 //             }
-//             if(st.empty()){
-//                 mp[nums2[i]] = -1;
-//             }
-//             else{
-//                 mp[nums2[i]] = st.top();
-//             }
-//             st.push(nums2[i]);
+//             result.push_back(nextGreater);
 //         }
-//         vector<int> ans;
-//         for(int x : nums1){
-//             ans.push_back(mp[x]);
-//         }
-//         return ans;
+//         return result;
 //     }
 // };
