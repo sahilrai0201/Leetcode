d24fe33a4a210d788616b370d51893eb0c2d8bce
Sahil Rai <sahilatwork21@gmail.com>
2026-07-26 15:26:10 +0530

    Time: 2 ms (79.12%) | Memory: 15.7 MB (64.22%) - LeetSync

---
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+
+        long long minSum = 0;
+        long long maxSum = 0;
+
+        vector<int> prevSmaller(n), nextSmaller(n);
+        vector<int> prevGreater(n), nextGreater(n);
+
+        stack<int> st;
+
+        // Previous Smaller
+        while(!st.empty()) st.pop();
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                st.pop();
+            }
+            prevSmaller[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        // Next Smaller
+        while(!st.empty()) st.pop();
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
+            }
+            nextSmaller[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        // Previous Greater
+        while(!st.empty()) st.pop();
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                st.pop();
+            }
+            prevGreater[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        // Next Greater
+        while(!st.empty()) st.pop();
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+            nextGreater[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        // Sum of Minimums
+        for(int i=0; i<n; i++){
+            long long left = i - prevSmaller[i];
+            long long right = nextSmaller[i] - i;
+            minSum += 1LL * nums[i] * left * right;
+        }
+
+        // Sum of Maximums
+        for(int i=0; i<n; i++){
+            long long left = i - prevGreater[i];
+            long long right = nextGreater[i] - i;
+            maxSum += 1LL * nums[i] * left * right;
+        }
+
+        return maxSum - minSum;
+    }
+};
