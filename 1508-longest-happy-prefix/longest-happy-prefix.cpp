c06c66168d6661f3582613a5245c04e05bbfc860
Sahil Rai <sahilatwork21@gmail.com>
2026-08-07 22:24:26 +0530

    Time: 10 ms (39.71%) | Memory: 23.4 MB (60.82%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     string longestPrefix(string s) {
-
         int n = s.size();
-
         vector<int> lps(n, 0);
 
         int len = 0;
         int i = 1;
 
         while (i < n) {
-
             if (s[i] == s[len]) {
                 len++;
                 lps[i] = len;
                 i++;
             }
             else {
-
                 if (len != 0)
                     len = lps[len - 1];
                 else {
