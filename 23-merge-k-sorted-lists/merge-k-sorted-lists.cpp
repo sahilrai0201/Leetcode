9cc27d8d2d7ca0d34721960418af730e53110aa9
Sahil Rai <sahilatwork21@gmail.com>
2026-08-22 00:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (66.22%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,36 +1,50 @@
 class Solution {
 public:
-    
-    struct Compare{
-        bool operator()(ListNode* a, ListNode* b){
+
+    // Custom comparator to make the priority_queue a min-heap
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
             return a->val > b->val;
         }
     };
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        // Min-heap storing nodes based on their values
         priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
 
-        for(ListNode* head : lists){
-            if(head != nullptr){
+        // Put the first node of every non-empty list into the heap
+        for (ListNode* head : lists) {
+            if (head != nullptr) {
                 pq.push(head);
             }
         }
 
+        // Dummy node to simplify building the result list
         ListNode dummy(0);
+
+        // tail always points to the last node of the merged list
         ListNode* tail = &dummy;
 
-        while(!pq.empty()){
+        // Continue until all nodes have been processed
+        while (!pq.empty()) {
+
+            // Get the node with the smallest value
             ListNode* curr = pq.top();
             pq.pop();
 
+            // Add the smallest node to the result list
             tail->next = curr;
             tail = curr;
 
-            if(curr->next != nullptr){
+            // If this node has a next node,
+            // add it to the heap for future comparison
+            if (curr->next != nullptr) {
                 pq.push(curr->next);
             }
         }
 
+        // dummy.next is the head of the merged linked list
         return dummy.next;
     }
 };
